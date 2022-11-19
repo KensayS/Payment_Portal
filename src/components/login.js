78e844c0d662187f39d8da9fa5acc514c599663d
Kensay Sato <kensaysato@gmail.com>
2022-11-19 17:30:44 -0500

    finished login

--- src/components/login.js
+++ src/components/login.js
@@ -1,13 +1,19 @@
 import React from "react"
 import "./login.css"
+import {useState} from "react"
 
 const Login = () => {
+
+
     return(
         <div className="cover">
-            <h1> Login </h1>
+            <h1> Customer Login </h1>
             <input type="text" placeholder="Customer ID" /> 
             <br></br>
             <input type="password" placeholder="Password" />
+
+            <div className="loginBTN">Login</div>
+
         </div>
     )
 }
