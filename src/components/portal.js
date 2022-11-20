226a6618d86348f096190bf8baec3ec69af0c888
Kensay Sato <kensaysato@gmail.com>
2022-11-19 23:37:18 -0500

    Portal Trial #1

--- src/components/portal.js
+++ src/components/portal.js
@@ -8,9 +8,9 @@ const Portal = () =>{
             <h2 id="header"> Name: Kensay Sato </h2>
             <h3 id="header"> Date: 11/19/2022</h3>
             <div className="paymentbox">
-                <b> Payment Due: $600.00</b>
+                <b> Payment Due:<br></br>$600.00</b>
                 <hr></hr>
-                <p id="date"> 
+                <p> 
                 Date Due:<br></br> 12/1/2022
                 </p>
             </div>
