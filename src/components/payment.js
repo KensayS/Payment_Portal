aaf8a4bede8628c19f6819bf6264802bf52bc3dd
Kensay Sato <kensaysato@gmail.com>
2022-11-20 00:33:53 -0500

    Payment page draft

--- src/components/payment.js
+++ src/components/payment.js
@@ -2,12 +2,41 @@ import React, {useState} from 'react'
 import "./payment.css"
 
 
-const Payment = () =>{
-    return(
-        <div>
-            <h1>PAYMENT</h1>
-        </div>
-)
-}
-
-export default Payment
+export default function Form() {
+    const [newCompany, setCompany] = useState("");
+    const [newPosition, setPosition] = useState("");
+    const [newLink, setNewLink] = useState("");
+    const [newDate, setNewDate] = useState("");
+    const [newNote, setNewNote] = useState("");
+  
+    return (
+          <form>
+            <input
+              value={newCompany}
+              onChange={(e) => setCompany(e.target.value)}
+              label="Company"
+            />
+            <input
+              value={newPosition}
+              onChange={(e) => setPosition(e.target.value)}
+              label="Job Title"
+            />
+            <input
+              value={newLink}
+              onChange={(e) => setNewLink(e.target.value)}
+              label="Job Link"
+            />
+            <input
+              type="date"
+              value={newDate}
+              onChange={(e) => setNewDate(e.target.value)}
+            />
+            <input
+              value={newNote}
+              onChange={(e) => setNewNote(e.target.value)}
+              label="Note"
+            />
+            <button type="submit"> Submit </button>
+          </form>
+    );
+  }
